5f033a1afd8ba90cc4e162901ccfc891347bb282
Howlin <Halen@BIG-CHUNGUS>
2025-08-04 02:19:54 -0400

    Add project files.

---
+++ dice game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace diceGame
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Die testDie = new();
+            Scoring testScoring = new(); // Scoring class is used to store rolls / check for scoring conditions.
+
+
+            while (true)
+            {
+                Console.WriteLine("\nPress R key to roll, or Q to quit. (Optionally, D to debug.)\n");
+                ConsoleKeyInfo rollKey = Console.ReadKey();
+                switch (rollKey.Key)
+                {
+                    case ConsoleKey.D:
+                        Console.WriteLine("ebug:"); //Spelled "ebug" intentionally, run the code and see why.
+                        Console.WriteLine("inPlay: " + testDie.inPlay);
+                        Console.WriteLine("rollStorage: ");
+                        foreach (var item in testScoring.rollStorage.Select((value, index) => new { value, index }))
+                        {
+                            Console.WriteLine($"index:{item.index} - value:{item.value}");
+                        }
+                        for (int i = 0; i < testScoring.rollStorage.Length; i++)
+                        {
+                            Console.WriteLine($"index: {i} - value: {testScoring.rollStorage[i]}");
+                        }
+                        break;
+                        
+                    case ConsoleKey.R:
+                        Console.WriteLine("olling dice..."); //Spelled "olling" intentionally.
+                        testDie.Randomized(); // Roll the die.
+                        break;
+
+                    case ConsoleKey.Q:
+                        Console.WriteLine("uiting program..."); //Spelled "uitig" intentionally.
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid key pressed.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ dice game/Scoring.cs
@@ -0,0 +1,44 @@
+﻿using diceGame;
+using System;
+using System.Threading.Tasks;
+
+namespace diceGame
+{
+    public class Scoring
+    {
+        public static Die valueOf = new();
+        public int[] rollStorage = new int[valueOf.inPlay];
+
+        //Consider the rest of the code below unfinished, im just laying down the foundations here.
+        public bool hasStraight = false;
+        public bool hasXOfKind = false;
+
+        public void Triplets()
+        {
+            // 3 of a kind 1s: 1,000.
+            // 3 of a kind 2-6 is 100x the number:
+            // (2 = 200, 3 = 300, 4 = 400, 5 = 500, 6 = 600)
+            int[] onesTriplet = { 1, 1, 1, };
+            int[] twoTriplet = { 2, 2, 2, };
+            int[] threeTriplet = { 3, 3, 3 };
+            int[] fourTriplet = { 4, 4, 4, };
+            int[] fiveTriplet = { 5, 5, 5, };
+            int[] sixTriplet = { 6, 6, 6, };
+            return;
+            // Theres probably a less unga-bunga way to do this. 
+        }
+
+        public void XOfAKind(int x)
+        {
+            // In the game Farkle, there are triplets and there are "x of a kind" scores.
+            // Within the "of a kind" category: 4 kind = 1k pts, 5 kind = 2k pts, and 6 kind = 3k pts.
+            // I make this distinction because 3 "of a kind" (triplet) rolls are scored on a different system.
+            // (See the Triplets method above)
+
+            // This method will check for x of a kind in the rollStorage.
+            // For example, if x = 4, it will check for quadruplets.
+            // If x = 5, it will check for quintuplets, etc.
+            return;
+        }
+    }
+}
